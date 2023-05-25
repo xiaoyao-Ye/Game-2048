b0c62bfed68a6f508466728e204090109d780d42
Ghosteye <yqcyecong@163.com>
2023-05-25 21:51:44 +0800

    style: 💄 Use dynamic concatenations
    unocss的动态样式需要配置才能生效

--- uno.config.ts
+++ uno.config.ts
@@ -3,4 +3,18 @@ import { defineConfig } from "unocss";
 
 export default defineConfig({
   // ...UnoCSS options
+  safelist: [
+    "#ccc0b3",
+    "#eee4da",
+    "#ede0c8",
+    "#f2b179",
+    "#f59563",
+    "#f67c5f",
+    "#f65e3b",
+    "#edcf72",
+    "#edcc61",
+    "#edc850",
+    "#edc53f",
+    "#edc22e",
+  ].map(color => `bg-${color}`),
 });
